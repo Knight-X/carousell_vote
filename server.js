4a97a34237b2c08eb19426c64f7c3b8bc8055c81
kazami <kazamizero@gmail.com>
2017-07-04 15:01:59 +0000

    first commit for create topic api

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var jsonParser = bodyParser.json({type: 'application/json'});
+
+var topic_count = 0
+var mem = {} 
+
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.render('hello', {
+        topics: mem,
+    });
+});
+app.post('/create', jsonParser, function(req, res) {
+    console.log(req.body);
+    var topicid = "id" + topic_count;
+    mem[topicid] = {title: req.body.topic, votes: "0"};
+    res.json({publish: 'y'});
+});
+
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("go");
+});
