cbc475fd43dc9ac521943283bc90218daba8e5ac
kazami <kazamizero@gmail.com>
2017-07-05 02:56:07 +0000

    complete the function that vote post api can increase the votes num of the topic

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var app = express();
 var jsonParser = bodyParser.json({type: 'application/json'});
 
 var topic_count = 0
-var mem = {} 
-
+var mem = [] 
+mem[0] = {title: "kai", votes: 5};
+mem[1] = {title: "gy", votes: 6};
+console.log(mem);
 app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
-    res.render('hello', {
+    res.render('index', {
         topics: mem,
     });
 });
@@ -21,8 +23,10 @@ app.post('/create', jsonParser, function(req, res) {
 
 app.post('/vote', jsonParser, function(req, res) {
     console.log(req.body);
-    //mem[id]['votes'] += 1;
-    res.json({m: 'g'});
+    var id = req.body.id;
+    mem[id]['votes'] += 1;
+    var v_num = mem[id]['votes'];
+    res.json({votes: v_num});
 });
 
 
