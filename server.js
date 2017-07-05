1eabc3df6c89ff7f0318913ffb05dfc9654d9868
kazami <kazamizero@gmail.com>
2017-07-05 04:06:10 +0000

    complete basic function for downvote the article and fix the name of api for upvoting

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var bodyParser = require('body-parser');
 var app = express();
 var jsonParser = bodyParser.json({type: 'application/json'});
 
-var topic_count = 0
 var mem = [] 
 mem[0] = {title: "kai", votes: 5};
 mem[1] = {title: "gy", votes: 6};
@@ -16,15 +15,21 @@ app.get('/', function(req, res) {
 });
 app.post('/create', jsonParser, function(req, res) {
     console.log(req.body);
-    var topicid = "id" + topic_count;
-    mem[topicid] = {title: req.body.topic, votes: "0"};
+    mem.push({title: req.body.topic, votes: 0});
     res.json({publish: 'y'});
 });
 
-app.post('/vote', jsonParser, function(req, res) {
+app.post('/upvote', jsonParser, function(req, res) {
     console.log(req.body);
     var id = req.body.id;
     mem[id]['votes'] += 1;
+    var v_num = mem[id]['votes'];
+    res.json({votes: v_num});
+});
+app.post('/downvote', jsonParser, function(req, res) {
+    console.log(req.body);
+    var id = req.body.id;
+    mem[id]['votes'] -= 1;
     var v_num = mem[id]['votes'];
     res.json({votes: v_num});
 });
